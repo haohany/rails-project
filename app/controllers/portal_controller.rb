811dd8f86415cfff377a7c0ec7651b5886db560c
Haohan <ysyanghaohan@gmail.com>
2015-06-12 04:41:52 -0700

    fix N+1 queries issue

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,5 +1,9 @@
 class PortalController < ApplicationController
   def show
-    @item_users = ItemUser.joins(:item, :user).paginate(page: params[:page], per_page: 10)
+    if logged_in?
+      @item_users = ItemUser.joins(:item, :user).includes(:user, item: :categories).paginate(page: params[:page], per_page: 10, total_entries: ItemUser.joins(:item, :user).count)
+    else
+      render 'show_login'
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @purchased_items = @user.items
+    @purchased_items = @user.items.includes(:categories)
 
-    purchased_categories = []
+    purchased_categories = @purchased_items.map { |item| item.categories } .flatten
 
-    @purchased_items.each do |item|
-      purchased_categories.concat(item.categories)
-    end
+    @recommended_items = Item.joins(:categories).where(categories: { id: purchased_categories.map(&:id) }).distinct.includes(:categories)
 
-    @recommended_items = []
-
-    purchased_categories.uniq.each do |category|
-      @recommended_items.concat(category.items)
-    end
-
-    @recommended_items.uniq!
     @recommended_items -= @purchased_items
 
     @recommended_items.sort! do |a, b|
